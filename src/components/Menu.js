e369f560b6d08f9823f7895f3d78f6c1dbd131de
RNO <arnaud.juracek@gmail.com>
2021-05-12 15:33:53 +0200

    Fix menu scroll on mobile

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,17 @@
 export default () => {
-  // Handle links
   const toggle = document.getElementById('toggleMenuVisibility')
+
+  // Disable scroll when open
+  toggle.addEventListener('change', e => {
+    document.body.classList.toggle('no-scroll', toggle.checked)
+  })
+
+  // Handle links
   const anchors = document.querySelectorAll('.menu a[href^="#"]')
   for (let anchor of Array.from(anchors)) {
     anchor.addEventListener('click', e => {
       toggle.checked = false
+      document.body.classList.remove('no-scroll')
     })
   }
 
